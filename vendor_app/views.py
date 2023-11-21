8a3cf6a70b35022546913f0d192833e4eef500dd
Prabaharan-nagappan <prabaharan.nagappan@gmail.com>
2023-11-21 07:51:26 +0000

    docker added

--- vendor_app/views.py
+++ vendor_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Vendor, PurchaseOrder
+from .serializers import VendorSerializer, PurchaseOrderSerializer
 
-# Create your views here.
+class VendorListCreateView(generics.ListCreateAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+class VendorRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+class PurchaseOrderListCreateView(generics.ListCreateAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+class PurchaseOrderRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
--- vendor_management/urls.py
+++ vendor_management/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for vendor_management project.
+from django.urls import path, include
+from vendor_app.views import VendorListCreateView, VendorRetrieveUpdateDeleteView, PurchaseOrderListCreateView, PurchaseOrderRetrieveUpdateDeleteView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+urlpatterns = [
+    path('api/vendors/', VendorListCreateView.as_view(), name='vendor-list-create'),
+    path('api/vendors/<int:pk>/', VendorRetrieveUpdateDeleteView.as_view(), name='vendor-retrieve-update-delete'),
+    path('api/purchase_orders/', PurchaseOrderListCreateView.as_view(), name='purchase-order-list-create'),
+    path('api/purchase_orders/<int:pk>/', PurchaseOrderRetrieveUpdateDeleteView.as_view(), name='purchase-order-retrieve-update-delete'),
+    # Add other URLs as needed
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('vendor_management.urls')),
 ]
+
+
